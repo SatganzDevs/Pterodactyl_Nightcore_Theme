01ff8d456104efed8f05834f1e89d036ccd0f64d
Kurniawan Satria <satganzdevs@gmail.com>
2024-12-31 11:48:47 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,7 +26,7 @@ installTheme(){
     echo -e "${GREEN}Removing old theme if exist${RESET}"
     rm -r Pterodactyl_Nightcore_Theme > /dev/null 2>&1
     echo -e "${GREEN}Download the Theme${RESET}"
-    git clone https://github.com/NoPro200/Pterodactyl_Nightcore_Theme.git > /dev/null 2>&1
+    git clone https://github.com/SatganzDevs/Pterodactyl_Nightcore_Theme.git > /dev/null 2>&1
     cd Pterodactyl_Nightcore_Theme > /dev/null 2>&1
     echo -e "${GREEN}Removing old theme resources if exist${RESET}"
     rm /var/www/pterodactyl/resources/scripts/Pterodactyl_Nightcore_Theme.css > /dev/null 2>&1
@@ -92,7 +92,7 @@ restoreBackUp(){
     yarn build:production > /dev/null 2>&1
     sudo php artisan optimize:clear > /dev/null 2>&1
 }
-echo "Copyright (c) 2024 Angelillo15 and NoPro200"
+echo "Copyright (c) 2024 SatganzDevs"
 echo "This program is free software: you can redistribute it and/or modify"
 echo ""
 echo ""
